7b710fc1aef976025bcbf3c086b2d474b78e2528
paola0210 <deiver.02otero@gmail.com>
2022-03-05 11:40:02 -0500

    configuracio
    cambio de ip

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/Proyecto/';
+$config['base_url'] = 'http://190.14.233.186/Proyecto/';
 
 /*
 |--------------------------------------------------------------------------
